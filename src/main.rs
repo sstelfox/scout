4acbd11861ddcbfa2ff50e191749bd7d5ec26b4b
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2018-05-22 20:14:54 -0400

    Add in a simple and stupid set of static pages we can use as a site reference

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,8 @@ extern crate env_logger;
 #[macro_use]
 extern crate log;
 
-use actix_web::{App, HttpRequest, middleware, server};
+use actix_web::{App, fs, HttpResponse, middleware, server};
 use dotenv::dotenv;
-
-fn index(_req: HttpRequest) -> &'static str {
-    "TODO: Serve file"
-}
 
 fn main() {
     dotenv().ok();
@@ -19,11 +15,16 @@ fn main() {
 
     let sys = actix::System::new("scout");
 
-    server::new(move ||
+    server::new(move || vec![
+        App::new()
+            .prefix("/api/v1")
+            .middleware(middleware::Logger::default())
+            .resource("/", |r| r.f(|_r| HttpResponse::Ok())),
+        // No logger for the static routes for now...
         App::new()
             .middleware(middleware::Logger::default())
-            .resource("/", |r| r.f(index))
-    ).bind("127.0.0.1:9292")
+            .handler("/", fs::StaticFiles::new("./static/").index_file("index.html")),
+    ]).bind("127.0.0.1:9292")
         .expect("Unable to bind to 127.0.0.1:9292")
         .start();
 
